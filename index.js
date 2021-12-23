57762a004d78d4917a296ce9c011436b9ea4e9c1
Peu <peustratt@gmail.com>
2021-12-23 17:18:16 -0300

    addedd deck generator method

--- index.js
+++ index.js
@@ -2,46 +2,64 @@ let player = {
     name: "Peter",
     chips: 140
 }
-let cards = []
+let deck = []
+let playerCards = []
 let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
-
-
+let playerCardsEl = document.getElementById("cards-el")
 let playerEL = document.getElementById("player-el")
+
 playerEL.textContent = player.name + ": $" + player.chips
 
-function getRandomCard() {
-    let randomNumber = Math.floor(Math.random() * 13) + 1
-    if (randomNumber === 1) {
-        return 11
-    } else if (randomNumber > 10) {
-        return 10
-    } else {
-        return randomNumber
+function generateDeck() {
+    var naipes = ['C', 'O', 'E', 'P']
+    var numeros = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+    var deck = []
+
+    for (var i=0; i < naipes.length; i++) {
+        for (var j=0; j < numeros.length; j++) {
+            deck.push(carta = {
+                naipe: naipes[i],
+                value: numeros[j],
+                name: numeros[j] + naipes[i]
+            })
+        }
     }
+    return deck
+}
+
+function getRandomCard() {
+    card = deck.pop()
+    if (card.value === 'J' || card.value === 'Q' || card.value === 'K') {
+        card.value = 10
+    } else if (card.value === 'A') {
+        card.value = 10
+    }
+    return card
 }
 
 function startGame() {
+    deck = generateDeck()
     isAlive = true
     hasBlackJack = false
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
+    playerCards = [firstCard, secondCard]
+    sum = firstCard.value + secondCard.value
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+    playerCardsEl.textContent = "Cards: "
+    for (let i = 0; i < playerCards.length; i++) {
+        playerCardsEl.textContent += playerCards[i].name + " "
     }
-    
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -58,9 +76,9 @@ function renderGame() {
 function newCard() {
     console.log(hasBlackJack)
     if (isAlive && !hasBlackJack) {
-        let card = getRandomCard()
-        sum += card
-        cards.push(card)
+        var card = getRandomCard()
+        sum += card.value
+        playerCards.push(card)
         renderGame()
     }
 }
